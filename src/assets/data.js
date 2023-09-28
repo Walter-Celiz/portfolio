72e5c4bca0207a6df26be7c7fd58e377c1d6237a
Wolz <wceliz.dev@gmail.com>
2023-09-28 10:09:48 -0300

    feat: core project v1

---
+++ src/assets/data.js
@@ -0,0 +1,110 @@
+export const socials = [
+  { logo: "bx bxl-github" },
+  { logo: "bx bxl-linkedin-square" },
+  { logo: "bx bx-envelope" },
+];
+
+export const info = [{ text: "About" }, { text: "Projects" }];
+
+export const technologies = [
+  {
+    text: "HTML",
+    logo: "bx bxl-html5",
+  },
+  {
+    text: "CSS",
+    logo: "bx bxl-css3",
+  },
+  {
+    text: "JavaScript",
+    logo: "bx bxl-javascript",
+  },
+  {
+    text: "TypeScript",
+    logo: "bx bxl-typescript",
+  },
+  {
+    text: "React",
+    logo: "bx bxl-react",
+  },
+  {
+    text: "NextJS",
+    logo: "bx bx-expand-horizontal",
+  },
+  {
+    text: "Astro",
+    logo: "bx bx-rocket",
+  },
+  {
+    text: "GitHub",
+    logo: "bx bxl-github",
+  },
+];
+
+export const projects = [
+  {
+    title: "Project VideoGames",
+    description:
+      "React, Redux, JS, CSS, HTML, PostgresSQL, Sequelize, Express",
+    image: "image-videogames",
+    color: "#f3c201",
+  },
+  {
+    title: "Henry Foods",
+    description:
+      "React, Redux, JS, CSS, HTML, PostgresSQL, Sequelize, Express",
+    image: "image-foods",
+    color: "#1629ab",
+  },
+  {
+    title: "snYKers",
+    description:
+      "React, Redux ToolKit, JS, CSS GRID, Bootstrap, HTML",
+    image: "image-snykers",
+    color: "#045fb7",
+  },
+  {
+    title: "Facebook Clone",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.",
+    image: "image-facebook-clone",
+    color: "#146866",
+  },
+  {
+    title: "Instagram Clone",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.",
+    image: "image-instagram-clone",
+    color: "#17193f",
+  },
+  {
+    title: "Slider",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.",
+    image: "image-slider",
+    color: "#bbbbbb",
+  },
+  {
+    title: "Spotify Clone",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.",
+    image: "image-spotify-clone",
+    color: "#49d53c",
+  },
+  {
+    title: "Youtube Clone",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.",
+    image: "image-youtube-clone",
+    color: "#f7030c",
+  },
+];
+
+export const buttons = [
+  {
+    github: "bx bxl-github",
+    demo: "bx bx-desktop",
+    back: "bx bxs-left-arrow-circle",
+    hrefgithub: "https://github.com/MilaDev-JS",
+  },
+];
